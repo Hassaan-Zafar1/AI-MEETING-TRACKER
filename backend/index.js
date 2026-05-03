29a152d37fade37db900ec56f2be1f3cd8e9c351
Hassaan Zafar <mhzafar07@gmail.com>
2026-05-03 16:48:36 +0500

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000',
-      process.env.CLIENT_URL,
-      'https://*.pages.dev',],
+    origin: [process.env.CLIENT_URL],
     credentials: true,
   },
 });
